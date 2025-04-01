b005176e58d57ba6eb2a066d5105c0156872b30c
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-03-31 23:19:30 -0300

    Create: Technology component & css

---
+++ src/components/Technology.jsx
@@ -0,0 +1,39 @@
+import { icons } from "./IconsExport";
+import '../styles/Technology.css'
+
+export const Technology = () => {
+
+        const skillsType= (type) =>icons.filter(skills => skills.type=== type)
+        const skillsLanguage= skillsType('Language')
+        const skillsDb= skillsType('db')
+        const skillsFrame= skillsType('frame')
+        const skillsother= skillsType('other')
+        const sourceImage=(source)=>(
+            source.map((sources,index)=>
+                <li key={index} className={`Skill-${sources.type}`}>
+                    <img src= {sources.src} alt={`Skill-${sources.name}`} className={`img-${sources.type}`}/>
+                </li>            
+            )
+        );      
+        return (
+            <>
+            <h1>Skills</h1>
+            <div className="div-list">
+                <ul className="first-list">
+                {sourceImage(skillsLanguage)}
+                {sourceImage(skillsDb)}
+                </ul>
+                <div>
+                <ul className="second-list">
+                {sourceImage(skillsFrame)}
+                {sourceImage(skillsother)}
+                </ul>
+                </div>
+            </div>
+            
+            </>
+        );
+    }
+
+
+export default Technology;
