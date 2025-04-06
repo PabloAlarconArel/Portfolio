552f89f757866f964061b9b646905ef199df90f6
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-04-05 23:32:26 -0300

    create: icon export & svg icons

---
+++ src/components/IconsExport.jsx
@@ -0,0 +1,16 @@
+
+export const icons =[    
+        {id:1, name:'Javascript', src:'../public/icons/javascript.svg',type:'Language'},
+        {id:2,name:'Python', src:'../public/icons/python.svg',type:'Language'},
+        {id:3,name:'Java', src:'../public/icons/java.svg',type:'Language'},
+        {id:4,name:'MongoDB', src:'../public/icons/mongodb.svg',type:'db'},
+        {id:5,name:'Postgres', src:'../public/icons/postgresql.svg',type:'db'},
+        {id:6,name:'SQL Server',src:'../public/icons/sql-server.svg',type:'db'},
+        {id:7,name:'Django',src:'../public/icons/django.svg',type:'frame'},
+        {id:8,name:'Flutter',src:'../public/icons/flutter.svg',type:'frame'},
+        {id:9,name:'React',src:'../public/icons/React_dark.svg',type:'other'},
+        {id:10,name:'Docker',src:'../public/icons/docker.svg',type:'other'},
+        {id:11,name:'GitHub',src:'../public/icons/github-mark.svg',type:'other'}
+    ];
+
+    
