238a6bc8777da03b55e7c158a5da1669037a0d8b
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-07-18 23:48:28 -0400

    Changes: add icone and title name in About and create css icon.

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import {Component} from 'react';
+import '../styles/header.css';
+
+class About extends Component {
+    
+    render() {
+        return (
+            <div className='div-about' id='About'>
+                <span>
+                    <img src='/public/icons/principal_icon.svg' alt='Avatar' className='avatar' />
+                    <h2>Pablo Alarcón</h2>
+                    <p> Backend developer and Data analytic from Santiago, Chile</p>
+                </span>
+                <br/>
+                <h1 >About Me</h1>
+                <div className='about-text'>
+                    <p>This section contains information about.. shfjhdshfj dsjfhshdf dshfhfjhf jhefhsjfhds
+                    sfhjdshfd shdghsjh s dhfjfjf sh djfh fjdsh hfdshj  sdk h sdkj hsdkf kshf hfds hffhjks dhçaçhdfhj
+                     sajsojwfds dksjfsjf dskjfjd kfdjfskaj kjsdfakjfkj dfksdfkjd  dsjdjfkjdçaj dkjdskjfskdjfsdkdjfds.</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default About;
