42f5ea5eb5ad063a2e8d5b191cfbf41a7f4d0a0c
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-09-22 07:40:03 -0300

    Add icons, and modificate src folder

--- src/components/IconsExport.jsx
+++ src/components/IconsExport.jsx
@@ -1,16 +1,18 @@
 
 export const icons =[    
-        {id:1, name:'Javascript', src:'../public/icons/javascript.svg',type:'Language'},
-        {id:2,name:'Python', src:'../public/icons/python.svg',type:'Language'},
-        {id:3,name:'Java', src:'../public/icons/java.svg',type:'Language'},
-        {id:4,name:'MongoDB', src:'../public/icons/mongodb.svg',type:'db'},
-        {id:5,name:'Postgres', src:'../public/icons/postgresql.svg',type:'db'},
-        {id:6,name:'SQL Server',src:'../public/icons/sql-server.svg',type:'db'},
-        {id:7,name:'Django',src:'../public/icons/django.svg',type:'frame'},
-        {id:8,name:'Flutter',src:'../public/icons/flutter.svg',type:'frame'},
-        {id:9,name:'React',src:'../public/icons/React_dark.svg',type:'other'},
-        {id:10,name:'Docker',src:'../public/icons/docker.svg',type:'other'},
-        {id:11,name:'GitHub',src:'../public/icons/github-mark.svg',type:'other'}
+        {id:1, name:'Javascript', src:'/icons/javascript.svg',type:'Language'},
+        {id:2,name:'Python', src:'/icons/python.svg',type:'Language'},
+        {id:3,name:'Java', src:'/icons/java.svg',type:'Language'},
+        {id:4,name:'MongoDB', src:'/icons/mongodb.svg',type:'db'},
+        {id:5,name:'PostgreSQL', src:'/icons/postgresql.svg',type:'db'},
+        {id:6,name:'SQL Server',src:'/icons/sql-server.svg',type:'db'},
+        {id:7,name:'Django',src:'/icons/django.svg',type:'frame'},
+        {id:8,name:'Flutter',src:'/icons/flutter.svg',type:'frame'},
+        {id:9,name:'Express',src:'/icons/Express.js_light.svg',type:'frame'},
+        {id:10,name:'Node.js',src:'/icons/nodejs.svg',type:'other'},
+        {id:11,name:'React',src:'/icons/React_dark.svg',type:'other'},
+        {id:12,name:'Docker',src:'/icons/docker.svg',type:'other'},
+        {id:13,name:'GitHub',src:'/icons/github-mark.svg',type:'other'}
     ];
 
     
