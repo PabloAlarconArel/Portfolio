d8078f2696ac8b05601cdca663c2e0cf769bd7e3
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-09-22 07:38:16 -0300

    Modificate select image condition

--- src/components/hooks/SetListImage.jsx
+++ src/components/hooks/SetListImage.jsx
@@ -1,70 +1,41 @@
-import {useEffect, useRef, useState} from 'react';
-import{data} from '/src/assets/data.jsx';
+import {useState} from 'react';
 import '/src/styles/setListImage.css';
-import PropTypes from 'prop-types';
 
+export const SetListImage = ({image}) => {
+    const [selectedIndex, setSelectedIndex]= useState(0);
+    const [selectedImage, setSelectedImage]=useState(image[0]);
 
-export const SetListImage = ({projectID}) => {
-    const listRef = useRef();
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const selectNewImage=(index,images,next = true) =>{
+        const condition = next ? selectedIndex < image.length - 1 : selectedIndex > 0;
+        const nextIndex = next ? (condition ? selectedIndex +1 : 0) : (condition ? selectedIndex -1: image.length - 1);
+        setSelectedImage(image[nextIndex]);
+        setSelectedIndex(nextIndex);
+    };   
+    const previous=()=>{
+        selectNewImage(selectedIndex,image,false)
 
-    useEffect(() => {
-        const listElement = listRef.current;
-        const imgNode = listElement.querySelectorAll("li > img")[currentIndex]
-
-        if (imgNode) {
-            imgNode.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-        
-    },[currentIndex]);
-
-    const scrollToImage = (direction) => {
-        if (direction === 'prev'){
-            setCurrentIndex(curr =>{
-                const isfirstImage = currentIndex === 0;
-                return isfirstImage ? 0 : curr - 1;
-            })
-        } else {
-            const isLastImage = currentIndex === data.length - 1;
-            if(!isLastImage){
-                setCurrentIndex(curr => curr + 1 );
-            }
-        }
+    }
+    const next = () => {
+        selectNewImage(selectedIndex,image)
     }
 
     return(
         <>
         <div className="main-container">
-            <div className='slider-container'>      
-                <div className='leftArrow' onClick={() => scrollToImage('prev')}> &#10092;</div>
-                <div className='rightArrow' onClick={() => scrollToImage('next')}> &#10093;</div>
+            <div className='slider-container'>
+                {(image.length) > 1 ? ( 
+                    <>
+                    <div className='leftArrow' onClick={previous}> &#10092;</div>
+                    <div className='rightArrow' onClick={next}> &#10093;</div>
+                    </>
+                    ): null}
                 <div className='container-images'>    
-                    <ul ref={listRef}>
-                        {data.filter(item => item.project === projectID).map((item) => {
-                            return <li key={item.id}>
-                                <img src={item.imgUrl} alt='imgproject' className='imageProject' />
-                                   </li>
-                        })}
-                    </ul>
+                    <img src={`/images/${selectedImage}`} alt='projectimg' className='imageProject'/>
                 </div>
-            </div>
-                <div className="dots-container">
-                {data.filter(item=> item.project === projectID).map((_, idx) => (
-                    <div key={idx}
-                        className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}>&#9865;
-                    </div>))}
-                </div>                                    
+            </div>                                   
         </div>
         </>
 
     );
 
 };
-
-SetListImage.propTypes = {
-  projectID: PropTypes.string.isRequired
-};
-
-export default SetListImage;
